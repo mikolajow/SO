f964cf0461753323f4040214ba0544b7926979e3
PAPAJ <mikolajkasperek@o2.pl>
2018-03-21 13:06:26 +0100

    trzy gotowe trzy zostaly

--- systemy 2/src/SCAN.java
+++ systemy 2/src/SCAN.java
@@ -14,29 +14,100 @@ public class SCAN {
 	
 	public int run() {	
 		
+		ArrayList<zgloszenie> listaAktualna = new ArrayList<>();
 		
+		Collections.sort(lista, new Comparator<zgloszenie>() {	//sortuje po czasie wejscia
+			@Override
+			public int compare(zgloszenie o1, zgloszenie o2) {
+				return o1.getCzasWejscia() - o2.getCzasWejscia();
+			}//koniec compare
+		});//koniec klasy zagniezdzonej
+		
+		int iloscZgloszenNaStart = lista.size()*8/10;
+		int counter = 0;
+		int iloscPozostalychZgloszen = lista.size() - iloscZgloszenNaStart;
+		
+		for ( int i = 0; i < iloscZgloszenNaStart; i++  ) {
+			listaAktualna.add(lista.get(i));				///dodaje w³¹cznie z elem ostatniem
+		}
 		
 		int przemieszczenia = 0;
-		int aktualnyIndex = lista.size()/2;							//zaczniemy ze srodka ( jak w ksi¹¿ce)
-		
-		
-		Collections.sort(lista, new Comparator<zgloszenie>() {	//sortuje po miejscu na dysku
+
+		Collections.sort(listaAktualna, new Comparator<zgloszenie>() {	//sortuje po miejscu na dysku
 			@Override
 			public int compare(zgloszenie o1, zgloszenie o2) {
 				return o1.getMiejsceNaDysku() - o2.getMiejsceNaDysku();
 			}//koniec compare
 		});//koniec klasy zagniezdzonej
 		
-		for (zgloszenie z : lista) {
-			System.out.println(z);
-		}
+		//System.out.println("Przed dodaniem ");
+		//for (zgloszenie z : listaAktualna) {
+		//	System.out.println(z);
+		//}
 		
-		//zak³adaj¹c kierunek pocz¹tkowy "w lewo" przemiescimy siê z miejsca na dysku w którym znajduje siê zg³oszenie 1 do miejsca "0"  dysku a potem do 
-		//miejsca w którym znajduje siê ostatnie zg³oszenie
 		
-		przemieszczenia = lista.get(aktualnyIndex).getMiejsceNaDysku() + lista.get( lista.size()-1 ).getMiejsceNaDysku();
-		// miejsce na dysku indexu aktualnego to liczba przemieszczen z aktualnego miejsca na miejsce "0" 
-		//dodaje do tego miejsce na którym znajduje siê ostatnie zg³oszenie
+		boolean czyDodane = false;
+		
+		int aktualnyIndex = listaAktualna.size()/2;	
+		String kierunek = "prawo";						//zak³adamy ze kierunkiem pocz¹tkowym bêdzie "prawo" ->
+		
+		while ( listaAktualna.size() != 1 ) {
+			
+			
+			if ( (listaAktualna.size() == 2 || counter == iloscPozostalychZgloszen) && !czyDodane ) {//dodaje elementy gdy lista jest prawie pusta lub gdy iloœc wykonanych zg³oszeñ
+				zgloszenie aktualnyElement = listaAktualna.get(aktualnyIndex);						//jest równa iloœci pozosta³ych i liscie 1
+				for ( int i = iloscZgloszenNaStart; i < lista.size(); i++ ) {
+					listaAktualna.add(lista.get(i));
+				}//koniec for
+				
+				Collections.sort(listaAktualna, new Comparator<zgloszenie>() {	//sortuje po miejscu na dysku
+					@Override
+					public int compare(zgloszenie o1, zgloszenie o2) {
+						return o1.getMiejsceNaDysku() - o2.getMiejsceNaDysku();
+					}//koniec compare
+				});//koniec klasy zagniezdzonej
+				aktualnyIndex = listaAktualna.indexOf(aktualnyElement);
+				czyDodane = true;
+				
+				//System.out.println("Przemieszczenia przed dodaniem = " + przemieszczenia);
+				//System.out.println("Po dodaniu ");
+				//for (zgloszenie z : listaAktualna) {
+				//	System.out.println(z);
+				//}
+				
+			}//koniec if
+			
+			
+			switch (kierunek) {
+			case "prawo" :
+				
+				if ( aktualnyIndex != listaAktualna.size()-1 ) {//jeœli nie jestesmy na ostatnim indeksie to
+					przemieszczenia = przemieszczenia + Math.abs( listaAktualna.get(aktualnyIndex).getMiejsceNaDysku() -listaAktualna.get(aktualnyIndex + 1).getMiejsceNaDysku()  );
+					listaAktualna.remove(aktualnyIndex);	//aktualny index bez zmian jako ze reszta listy przesunie sie o 1
+				}//koniec if
+				else {
+					kierunek = "lewo";
+				}//koniec else
+				
+				break;
+			default:
+				
+				if ( aktualnyIndex == 0 ) {//jeœli jesteœmy na pierwszym indeksie
+					kierunek = "prawo";
+				}//koniec if
+				else {
+					przemieszczenia = przemieszczenia + Math.abs( listaAktualna.get(aktualnyIndex).getMiejsceNaDysku() -listaAktualna.get(aktualnyIndex -1).getMiejsceNaDysku()  );			
+					listaAktualna.remove(aktualnyIndex);	
+					aktualnyIndex--;
+				}//koniec else
+				
+				break;
+			}//koniec switch	
+			
+			counter++;
+		}//koniec while
+		
+		//System.out.println("Licznik =  " + counter);
 		
 		return przemieszczenia;
 	}//koniec run
--- systemy 2/src/SSTF.java
+++ systemy 2/src/SSTF.java
@@ -16,25 +16,65 @@ public class SSTF {
 		
 		ArrayList<zgloszenie> listaAktualna = new ArrayList<>();
 		
+		Collections.sort(lista, new Comparator<zgloszenie>() {	//sortuje po czasie wejscia
+			@Override
+			public int compare(zgloszenie o1, zgloszenie o2) {
+				return o1.getCzasWejscia() - o2.getCzasWejscia();
+			}//koniec compare
+		});//koniec klasy zagniezdzonej
+		
+		int iloscZgloszenNaStart = lista.size()*8/10;
+		int counter = 0;
+		int iloscPozostalychZgloszen = lista.size() - iloscZgloszenNaStart;
+		
+		for ( int i = 0; i < iloscZgloszenNaStart; i++  ) {
+			listaAktualna.add(lista.get(i));				///dodaje w³¹cznie z elem ostatniem
+		}
 		
 		int przemieszczenia = 0;
-		int aktualnyIndex = lista.size()/2;								//zaczniemy ze srodka ( jak w ksi¹¿ce)
-		
-		
-		Collections.sort(lista, new Comparator<zgloszenie>() {	//sortuje po miejscu na dysku
+
+		Collections.sort(listaAktualna, new Comparator<zgloszenie>() {	//sortuje po miejscu na dysku
 			@Override
 			public int compare(zgloszenie o1, zgloszenie o2) {
 				return o1.getMiejsceNaDysku() - o2.getMiejsceNaDysku();
 			}//koniec compare
 		});//koniec klasy zagniezdzonej
 		
-		for ( zgloszenie z : lista ) {
-			listaAktualna.add(z);
-		}
+		//System.out.println("Przed dodaniem ");
+		//for (zgloszenie z : listaAktualna) {
+		//	System.out.println(z);
+		//}
 		
 		
+		boolean czyDodane = false;
+		
+		int aktualnyIndex = listaAktualna.size()/2;								//zaczniemy ze srodka ( jak w ksi¹¿ce)
 		
 		while ( listaAktualna.size() != 1) {		//bo jak zostanie 1 zgloszenie to konczymy ( g³owica juz sie nie przemieszcza )
+			
+			if ( (listaAktualna.size() == 2 || counter == iloscPozostalychZgloszen) && !czyDodane ) {//dodaje elementy gdy lista jest prawie pusta lub gdy iloœc wykonanych zg³oszeñ
+				zgloszenie aktualnyElement = listaAktualna.get(aktualnyIndex);						//jest równa iloœci pozosta³ych i liscie 1
+				for ( int i = iloscZgloszenNaStart; i < lista.size(); i++ ) {
+					listaAktualna.add(lista.get(i));
+				}//koniec for
+				
+				Collections.sort(listaAktualna, new Comparator<zgloszenie>() {	//sortuje po miejscu na dysku
+					@Override
+					public int compare(zgloszenie o1, zgloszenie o2) {
+						return o1.getMiejsceNaDysku() - o2.getMiejsceNaDysku();
+					}//koniec compare
+				});//koniec klasy zagniezdzonej
+				aktualnyIndex = listaAktualna.indexOf(aktualnyElement);
+				czyDodane = true;
+				
+				//System.out.println("Po dodaniu ");
+				//for (zgloszenie z : listaAktualna) {
+				//	System.out.println(z);
+				//}
+				
+			}//koniec if
+			
+			
 			
 			if( aktualnyIndex != 0 && aktualnyIndex != (listaAktualna.size()-1) ) {
 				if ( Math.abs(listaAktualna.get(aktualnyIndex).getMiejsceNaDysku() - listaAktualna.get(aktualnyIndex-1).getMiejsceNaDysku() ) < Math.abs(listaAktualna.get(aktualnyIndex).getMiejsceNaDysku() - listaAktualna.get(aktualnyIndex+1).getMiejsceNaDysku() ) ) {					
@@ -58,6 +98,7 @@ public class SSTF {
 				listaAktualna.remove(aktualnyIndex);
 				aktualnyIndex--;
 			}//koniec else dla indexu równego size-1
+			counter++;
 		}//koniec while
 		return przemieszczenia;
 	}//koniec run
